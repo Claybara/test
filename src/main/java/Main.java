76d66e0e40437b746c86d77c08dca266c560cb00
Claybara <cjm18531@gmail.com>
2020-11-08 09:02:14 +0900

    d

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String args[]) {
-        System.out.println("test");
-        System.out.println("test");
-        System.out.println("test");
-        System.out.println("test");
-        System.out.println("test");
+        System.out.println("test1");
+        System.out.println("test2");
+        System.out.println("test3");
+        System.out.println("test4");
+        System.out.println("test5");
     }
 }
 
